247afba839eba2ac763e3035f8422210c75ffc98
null <null>
2024-09-11 10:24:16 +0800

    First Commit

---
+++ Module03Exercise01/View/LoginPage.xaml.cs
@@ -0,0 +1,19 @@
+using Module03Exercise01.ViewModel;
+using Module03Exercise01.Services;
+
+
+namespace Module03Exercise01.View;
+
+public partial class LoginPage : ContentPage
+{
+    private readonly IMyService _myService; //Field for the Service
+    public LoginPage(IMyService myService)
+    {
+        InitializeComponent();
+        BindingContext = new EmployeeLoginViewModel();
+        _myService = myService;
+
+        var message = _myService.GetMessage();
+        MyLabel.Text = message;
+    }
+}
